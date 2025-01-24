0510b25a1c79d35fe863681efbdef5916c7beb03
IronTeacup <80750613+IronTeacup@users.noreply.github.com>
2025-01-24 19:05:21 +0000

    Basic Movement Completed

--- Don't Burst my Bubble/Assets/Scripts/PlayerMovement.cs
+++ Don't Burst my Bubble/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float SPEED;
+    public int moveHorizontal;
+    public int moveVertical;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        SPEED = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            // Set move direction up
+            moveVertical = 1;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            moveVertical = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // Set move direction down
+            moveVertical = -1;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            moveVertical = 0;
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // Set move direction left
+            moveHorizontal = -1;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            moveHorizontal = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            // Set move direction right
+            moveHorizontal = 1;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            moveHorizontal = 0;
+        }
+
+        rb.velocity = new Vector2 (moveHorizontal * SPEED, moveVertical * SPEED);
     }
 }
